738aa700a60acd14aa7cfcf8ea50a132f4f6ea92
Luiza Almeida <113039683+Lu-Almeida@users.noreply.github.com>
2025-06-30 19:15:55 -0300

    docs: update Contador.java

--- src/Contador.java
+++ src/Contador.java
@@ -9,6 +9,7 @@ public class Contador {
         int parametroDois = terminal.nextInt();
 
         try{
+            //chamando o método contendo a lógica de contagem
             contar(parametroUm,parametroDois);
         } catch (ParametrosInvalidosException e) {
             // imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
